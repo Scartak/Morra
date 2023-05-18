45d7d991a6c9a9c312a4fa237ed81c216712a9b5
Saarthak Negi <negisarthak03@gmail.com>
2023-05-19 09:20:48 +1200

    Implemented start round and ask input in morra

--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -6,9 +6,17 @@ public class Morra {
 
   public Morra() {}
 
-  public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {}
+  public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {
+    MessageCli.WELCOME_PLAYER.printMessage(options);
+  }
 
-  public void play() {}
+  public void play() {
+    MessageCli.START_ROUND.printMessage("1");
+    MessageCli.ASK_INPUT.printMessage();
+    String input = Utils.scanner.nextLine();
+    
+
+  }
 
   public void showStats() {}
 }
